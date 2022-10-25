c3ac9de93956948111056e760813d792d9c0216f
Andressa <andressa.94.michalski@gmail.com>
2022-10-24 22:36:33 -0300

    (feat)navigationStudy - storyboard/segue/navigationController -  adding names to navigation bar at storyboar

--- studies/studies/ViewController.swift
+++ studies/studies/ViewController.swift
@@ -11,20 +11,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func goScreenTwoButton(_ sender: UIButton) {
         performSegue(withIdentifier: "goScreenTwo", sender: nil)
     }
-    // identifier is from xcreen
+    // identifier is from screen
     // sender is a way to sand info to another screen
     
     @IBAction func goScreenThreeButton(_ sender: UIButton) {
         performSegue(withIdentifier: "goScreenThree", sender: nil)
     }
     
-
-
 }
 
